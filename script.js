a63f8699bffd53592ae7db896f40c37a8d2210a9
shubham <shubhamaery10@gmail.com>
2018-07-28 00:27:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var color;
 
-function randomColor() {
-    color =  '#' + Math.random().toString(16).slice(2, 8);  // Random number converted to hexadecimal with toString(16) and then slice to make it a 6 digit number. like fe2e4d or f4e22e 
+
+function randomColor() // return a random hexadecimal number
+{
+  return '#' + Math.random().toString(16).slice(2, 8);  // Random number converted to hexadecimal with toString(16) and then slice to make it a 6 digit number. like fe2e4d or f4e22e 
 };
 
 
 
-var change = document.getElementById('color_change');
+var change = document.getElementById('color_change'); // initialized a variable change to button with id 'color change'  
 
-change.addEventListener('çlick', function() {
-
-    document.getElementById('random_background').style.backgroundColor  = "color" ; 
-
+change.addEventListener('click', function() // anonymous function that changes the background color of body with each click event  
+{
+    document.getElementById('random_background').style.backgroundColor  = randomColor() ;  
 });
 
 
